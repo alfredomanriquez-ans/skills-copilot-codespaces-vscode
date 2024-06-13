dc6381684dfc8d010882ccfaee23de1fc4386501
Alfredo Manriquez Fuentes <83409009+alfredomanriquez-ans@users.noreply.github.com>
2024-06-13 21:28:25 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,16 +1,33 @@
-// Create Web Server with Node.js
-// Create a comments.js file that will serve as a module that exports a single function that will handle incoming comments.
-// This function will accept an array of comments as a parameter and return a string of HTML representing the comments.
-// The comments will be represented as objects with name and message properties.
+// Create Web Server
+// http://expressjs.com/
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var fs = require('fs');
 
-// comments.js
-module.exports = function(comments) {
-  var commentHtml = '<div id="comments"><ul>';
-  comments.forEach(function(comment) {
-    commentHtml += '<li>' + comment.name + ': ' + comment.message + '</li>';
+// Create Web Server
+// http://expressjs.com/
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/comments', function(req, res) {
+  fs.readFile(__dirname + '/comments.json', 'utf8', function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.setHeader('Content-Type', 'application/json');
+    res.send(data);
   });
-  commentHtml += '</ul></div>';
-  return commentHtml;
-};
+});
 
+app.post('/comments', function(req, res) {
+  fs.readFile(__dirname + '/comments.json', 'utf8', function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    var comments = JSON.parse(data);
+    var newComment = {
+      id: Date.now(),
 
